18e0134f43b2a0690a7d4aee98a3544f6c567c1b
jnery <joaonery1@gmail.com>
2020-01-14 22:12:27 +0100

    - Port changed back to 8080 - .gitigore added

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const ws = require('express-ws')(app);
+const wss = require('express-ws')(app);
 
 const camera = require('./camera');
 
-const port = 3000;
+const port = 8080;
 
 app.use((req, res, next) => {
     console.info(`Method: ${req.method} url: ${req.originalUrl}`)
 });
 
 app.use(cors());
-
 camera.startVideoStream(app);
 
 app.listen(port, () => {
